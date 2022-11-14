8452f78a0d51818807e8e90b28d43af50989eb7d
sreadytocode <sara.younis2303@gmail.com>
2022-11-14 09:50:21 +0000

    Updated

--- client/src/Components/OrganList.js
+++ client/src/Components/OrganList.js
@@ -1,19 +1,17 @@
 import React from "react";
 import Button from "./Re-usable/Button";
 import { Link } from "react-router-dom";
-import { organById } from './OrgansService.js'
-import { useParams } from "react-router-dom";
-import OrganSelect from "./OrganSelect";
+
 
 const OrganList = ({organs, fetchOrganId}) => {
-    // const { id } = useParams()
+    
 
     const organList = organs.map(organ => {
-        
+
         return (
             <div key={organ._id}>
-            <Link to="/:id"><Button onClick={() => fetchOrganId(organ._id)} text={organ.name} value={organ._id}/></Link>
-        </div>)
+                <Link to="/:id"><Button onClick={() => fetchOrganId(organ._id)} text={organ.name} value={organ._id}/></Link>
+            </div>)
     })
 
 
--- client/src/Components/OrganSelect.js
+++ client/src/Components/OrganSelect.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+
 
 const OrganSelect = ({organ}) => {
     
@@ -7,7 +7,7 @@ const OrganSelect = ({organ}) => {
     return ( 
         <div>
             <h2>{organ.name}</h2>
-            <img src={organ.diagram} alt="Organ-diagram" width="350"/>
+                <img src={organ.diagram} alt="Organ-diagram" width="350"/>
         </div>
      );
 }
