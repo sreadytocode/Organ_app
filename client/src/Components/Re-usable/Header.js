627f374112d5e3acd412b95241d5a739998d01e7
Takako Onozuka <onozukatakako@gmail.com>
2022-11-17 09:43:21 +0000

    no need to merge

--- client/src/Components/Re-usable/Header.js
+++ client/src/Components/Re-usable/Header.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Link } from "react-router-dom";
 import OrgansContainer from "../../Containers/OrgansContainer";
 import { organById } from "../OrgansService";
+import './bbc.jpg'  
 
 const Header = () => {
     const Header = styled.header`
@@ -13,17 +14,14 @@ const Header = () => {
 
   const HeaderGroup = styled.hgroup `
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     margin: 0;
     padding-left: 30px;
     padding-bottom: 10px;
+    justify-content: center;
 
   `
-  // const InputIcon = styled.input`
-  //     margin: ${(props)=> props.margin}
-  //     height: ${(props)=> props.height}
-  //     padding: ${(props)=> props.padding}
-  // `
+
   return (
     <div>
     <Header>
@@ -31,18 +29,13 @@ const Header = () => {
           <h1><img src='https://cdn-icons-png.flaticon.com/512/4556/4556999.png' width='45px' height='45px'></img> Organ  <img src='https://cdn-icons-png.flaticon.com/512/2492/2492923.png' width='45px' height='45px'></img>  Learning  <img src='https://cdn-icons-png.flaticon.com/512/7292/7292483.png' width='45px' height='45px'></img> Guide
           </h1>
         </HeaderGroup>
+        {/* <HeaderGroup> 
+          <img src='https://1000logos.net/wp-content/uploads/2016/10/BBC-Logo-1.png' width='150px' height='50px'></img> 
+          <h1>  Organ Learning Guide 
+          </h1>
+        </HeaderGroup> */}
 
-      {/* <nav>
-        <ul>
-          <li><Link to="/">View Stock</Link></li>
-        </ul>
-
-
-      </nav>
-      <div id="user-info">
-        <h3>Welcome back </h3>
-        <h4>You have items in your basket</h4>
-        <Link to="/basket">Go to Basket</Link> */}
+ 
 
       </Header>
 
